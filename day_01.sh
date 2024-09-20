e641f9cbd9bfbee93775d3dd15fa6f1c16494b33
AdityaKiran876 <adikiran876@gmail.com>
2024-09-20 18:22:33 +0000

    Added first script of Day_01

---
+++ day_01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Task 1
+#Created a script which adds two numbers
+#This script will also show the hostname and current date
+
+#Task 3, 4, 5
+a=$(date)
+b=$(hostname)
+pwd
+c=5
+d=7
+e=$((c + d))
+
+#Task 2
+echo "$a"
+echo "$b"
+echo "The sum of $c and $d is : $e"
+
+#Task 6
+# Lists the files with the particular extension
+
+ls *.sh
+
