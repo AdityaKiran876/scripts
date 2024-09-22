a8a3208678766665f4341cd78373c27db458f524
AdityaKiran876 <adikiran876@gmail.com>
2024-09-22 02:43:15 +0000

    Added script for dat_02part1

---
+++ explorer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+<<Comments
+This script displays all the files in the current directory.
+It also will read input from a user and display the number of characters in the prompt entered by the user
+To use this script give command ./explorer.sh
+Comments
+
+echo "Welcome to the Interactive File and Directory Explorer!"
+
+#This function shows the files and directories and their size
+fileslisting() {
+
+	echo "Files and directories in current path :"
+
+	ls -sh | awk 'NR>1 {print "-"$2 " ("$1")"}'
+}
+
+
+#Taking continuous loop with true
+while true
+do
+
+	fileslisting
+	
+#Reading input from user to check if the user wants to continue or exit
+	read -p "Do you want to see the files again? If yes type y if no type n: " option
+
+	if [ $option == "y" ]; then
+		continue
+
+	elif [ $option == "n" ]; then
+		echo "...."
+#Exiting from the loop 
+		break
+	fi
+	break
+done
+
+
