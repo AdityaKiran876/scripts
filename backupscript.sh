6c249b88aaee4de5f6fae8e27a8a4ec3a6a0fb35
AdityaKiran876 <adikiran876@gmail.com>
2024-09-22 03:25:29 +0000

    Added backup script

---
+++ backupscript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+<<Readme
+This script takes backup of any given directory.
+Usage:
+./backupscript.sh <path of directory>
+Readme
+src_dir=$1
+target_dir=$2
+timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+backup_dir="${target_dir}/backup_${timestamp}"
+
+function create_backup {
+	
+	zip -r "${backup_dir}.zip" "${src_dir}" > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Backup is completed"
+	else
+		echo "Backup was not taken for $timestamp"
+	fi
+}
+function backup_rotation {
+	backups=($(ls -t "$target_dir/backup_"*.zip))
+	if [ "${#backups[@]}" -gt 5 ]; then
+		backupstoremove=("${backups[@]:5}")
+		for backup in "${backupstoremove[@]}";
+		do
+			rm "$backup"
+		done
+	fi
+
+
+}
+create_backup
+backup_rotation
