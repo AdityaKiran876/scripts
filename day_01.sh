8917bf340d400db6636e4440ca2a10ca0873e792
Voleti Aditya Kiran <163765797+AdityaKiran876@users.noreply.github.com>
2024-09-24 09:22:06 +0530

    Updated comments in day_01.sh

--- day_01.sh
+++ day_01.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#Task 1
+#Task 1 : Comments
 #Created a script which adds two numbers
 #This script will also show the hostname and current date
 
-#Task 3, 4, 5
+## Task  5: Using Built-in Variables
 a=$(date)
 b=$(hostname)
 pwd
+# Task  3 and 4: Using Variables
 c=5
 d=7
 e=$((c + d))
@@ -16,7 +17,7 @@ echo "$a"
 echo "$b"
 echo "The sum of $c and $d is : $e"
 
-#Task 6
+#Task  6: Wildcards
 # Lists the files with the particular extension
 
 ls *.sh
